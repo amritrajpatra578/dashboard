106e119c4d4f1ecf3188e312f1a30b595466ec88
amritraj <amritrajpatra578@gmail.com>
2025-07-11 02:39:42 +0530

    use getServerSideProps in details page

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,16 +1,30 @@
+// pages/dashboard.tsx
+import { GetServerSideProps } from "next";
 import CarsTable from "@/components/CarsTable";
 import PageSidebar from "@/components/PageSidebar";
+import { Car } from "@/car";
+import axios from "axios";
 import { Box } from "@chakra-ui/react";
-import { FunctionComponent } from "react";
 
-const TablePage: FunctionComponent = () => {
+interface Props {
+  initialCars: Car[];
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await axios.get("http://localhost:3000/api/listings"); // Use absolute path in production
+  return {
+    props: {
+      initialCars: res.data,
+    },
+  };
+};
+
+export default function Dashboard({ initialCars }: Props) {
   return (
     <PageSidebar>
       <Box ml={{ base: 0, md: 60 }} p="4" color="black">
-        <CarsTable />
+        <CarsTable initialCars={initialCars} />
       </Box>
     </PageSidebar>
   );
-};
-
-export default TablePage;
+}
