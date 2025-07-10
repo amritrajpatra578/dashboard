fb41e9c10fdbf86cc2a6156c726177d0a72c9490
amritraj <amritrajpatra578@gmail.com>
2025-07-11 02:41:50 +0530

    use custoom hook to get car data for edit page

--- src/pages/details/[id].tsx
+++ src/pages/details/[id].tsx
@@ -1,39 +1,36 @@
-import { Car, cars } from "@/car";
-import CarConfigForm from "@/components/CarConfigForm";
-import LoadingScreen from "@/components/LoadingScreen";
-import PageSidebar from "@/components/PageSidebar";
-import { Box } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { FunctionComponent, useMemo } from "react";
+import { useEffect, useState } from "react";
+import { Box } from "@chakra-ui/react";
+import { Car, cars } from "@/car";
+import PageSidebar from "@/components/PageSidebar";
+import CarConfigForm from "@/components/CarConfigForm";
+import { useCarStorage } from "@/hooks";
 
-const EditPage: FunctionComponent = () => {
-  const { query, isReady } = useRouter();
+export default function EditPage() {
+  const router = useRouter();
+  const { isReady, query } = router;
   const id = Number(query.id);
 
-  if (!isReady) {
-    return <LoadingScreen />;
-  }
+  const { getCarById } = useCarStorage(cars); // use default cars only if nothing is in localStorage
+  const [car, setCar] = useState<Car | null>(null);
 
-  const carConfig = useMemo(() => {
-    return cars.find((car) => car.id === id);
+  useEffect(() => {
+    if (isReady && !isNaN(id)) {
+      const found = getCarById(id);
+      setCar(found || null);
+
+      console.log({ found });
+    }
   }, [id, isReady]);
 
-  const defaultCar: Car = carConfig || {
-    brand: "BMW",
-    id: 20,
-    model: "m5",
-    price: 1800000,
-    type: "suv",
-    status: "rejected",
-  };
+  if (!isReady) return <Box p="8">Loading...</Box>;
+  if (!car) return <Box p="8">Car not found</Box>;
 
   return (
     <PageSidebar>
       <Box ml={{ base: 0, md: 60 }} p="4" color="black">
-        <CarConfigForm carConfig={defaultCar} />
+        <CarConfigForm carConfig={car} />
       </Box>
     </PageSidebar>
   );
-};
-
-export default EditPage;
+}
